bc7573bbb713bf2e525c05980f21826b37c8d8f5
UnitedCatdom <kgoszczycki3636@gmail.com>
2023-01-22 16:53:33 +0100

    Apply WM size to DE's config and add status bar

--- system/environment/desktop/everest/init.js
+++ system/environment/desktop/everest/init.js
@@ -12,7 +12,7 @@ function init() {
 
     //================================//
     // Get the desktop element
-    let desktop = document.querySelector("div#desktop");
+    let desktop = document.querySelector("body > div#desktop");
 
     //================================//
     // Initialize CSS
@@ -28,30 +28,34 @@ function init() {
             "val": env_cfg.taskbar.appearance.height
         }, 
         {
-            "name": "[--wallpaper-fit]",
-            "val": env_cfg.wallpaper.fit
+            "name": "[--status_bar-width]",
+            "val": env_cfg.status_bar.width
         }, 
         {
-            "name": "[--colors--taskbar]",
-            "val": colors.taskbar
+            "name": "[--wallpaper-fit]",
+            "val": env_cfg.wallpaper.fit
         }
     ];
     for (const variablei in cssvarialbes) {
         let variable = cssvarialbes[variablei];
         for (let i = 0; i < (stylesheet.match(/--/g) || []).length; i++) {
-            if (variable.name.match("colors")) {
-                let var_name_split = variable.name.split("--")[2].replace("]", "").split("-");
-                variable.val = "";
-                let color_var = "colors.";
-                for (let vnsi = 0; vnsi < var_name_split.length; vnsi++) {
-                    color_var += var_name_split[vnsi];
-                    if (var_name_split.length-1 < vnsi) color_var += ".";
-                };
-                variable.val = eval(color_var);
-            };
             stylesheet = stylesheet.replace(variable.name, String(variable.val));
         };
     };
+    if (stylesheet.match("--colors--")) {
+        let var_colors_split = stylesheet.split("[--colors--");
+        for (let i = 1; i < (var_colors_split.length); i++) {
+            let color_vars = var_colors_split[i].split("]")[0].split("-");
+            let color_var = "colors";
+            for (let cvari in color_vars) {
+                let cvar = color_vars[cvari];
+                color_var += `.${cvar}`;
+            };
+            let var_color = eval(color_var);
+            stylesheet = stylesheet.replace(`[--colors--${color_vars.join("-")}]`, var_color);
+        };
+    }
+    
 
     // Set the stylesheet content and append it to document
     desktop_env_css.textContent = stylesheet;
@@ -71,14 +75,127 @@ function init() {
     desktop.appendChild(wallpaper);
 
     //================================//
+    // Initialize status bar
+    let status_bar = document.createElement("div");
+    status_bar.id = "everest_desktop_environment__status_bar";
+
+    let status_bar_tray = document.createElement("div");
+    status_bar_tray.id = status_bar.id + "_tray";
+    status_bar_tray.className = "tray";
+
+    let status_bar_other = document.createElement("div");
+    status_bar_other.id = status_bar.id + "_others";
+    status_bar_other.className = "others";
+
+    // Initialize status bar tray
+    (function() {
+        // WIFI
+        let wifi = document.createElement("button");
+        wifi.id = status_bar_tray.id + "__wifi";
+        wifi.className = "wifi";
+
+        let wifi_icon = document.createElement("img");
+        wifi_icon.id = wifi.id + "_icon";
+        wifi_icon.src = `system/icons/tray/${wifi.className}.png`;
+
+        wifi.appendChild(wifi_icon);
+
+        // VOLUME
+        let volume = document.createElement("button");
+        volume.id = status_bar_tray.id + "__volume";
+        volume.className = "volume";
+
+        let volume_icon = document.createElement("img");
+        volume_icon.id = wifi.id + "_icon";
+        volume_icon.src = `system/icons/tray/${volume.className}.png`;
+
+        volume.appendChild(volume_icon);
+
+        // BATTERY
+        let battery = document.createElement("button");
+        battery.id = status_bar_tray.id + "__battery";
+        battery.className = "battery";
+
+        let battery_icon = document.createElement("img");
+        battery_icon.id = battery.id + "_icon";
+        battery_icon.src = `system/icons/tray/${battery.className}.png`;
+
+        battery.appendChild(battery_icon);
+
+        // Append buttons
+        status_bar_tray.appendChild(wifi);
+        status_bar_tray.appendChild(volume);
+        status_bar_tray.appendChild(battery);
+    })();
+
+    // Initialize status bar others
+    (function(){
+        // NOTIFICATIONS
+        let notifs = document.createElement("button");
+        notifs.id = status_bar_other.id + "__notifs";
+        notifs.className = "notification";
+
+        let notifs_icon = document.createElement("img");
+        notifs_icon.id = notifs.id + "_icon";
+        notifs_icon.src = `system/icons/tray/${notifs.className}.png`;
+
+        notifs.appendChild(notifs_icon);
+
+        // Append buttons
+        status_bar_other.appendChild(notifs);
+    })();
+
+    status_bar.appendChild(status_bar_tray);
+    status_bar.appendChild(status_bar_other);
+    desktop.appendChild(status_bar);
+
+    //================================//
+    // Initialize start menu
+    let start_menu = document.createElement("div");
+    start_menu.id = "everest_desktop_environment__start_menu";
+    start_menu.style.display = "none";
+
+    desktop.appendChild(start_menu);
+
+    // Hide menu when clicked anywhere
+    document.addEventListener("click", (e) => {
+        start_menu.style.opacity = "0";
+        setTimeout(() => {
+            start_menu.style.display = "none";
+        }, 100);
+    });
+
+    //================================//
     // Initialize the taskbar
     let taskbar = document.createElement("div");
     taskbar.id = "everest_desktop_environment__taskbar";
 
     //================================//
+    // Initialize taskbar clock
+    let taskbar_clock = document.createElement("div");
+    taskbar_clock.id = taskbar.id + "_clock";
+    taskbar_clock.textContent = String(Date.now());
+
+    taskbar.appendChild(taskbar_clock);
+
+    //================================//
     // Initialize taskbar buttons
     let taskbar_meta = document.createElement("button");
-    taskbar_meta.id = "everest_desktop_environment__taskbar_meta";
+    taskbar_meta.id = taskbar.id + "_meta";
+
+    // Open start menu on click
+    taskbar_meta.addEventListener("click", () => {
+        // Check if menu is visible
+        // If true, hide it
+        // Else show it
+        if (start_menu.style.display == "none")
+            setTimeout(() => {
+                start_menu.style.display = "unset";
+                setTimeout(() => {
+                    start_menu.style.opacity = "1";
+                }, 1);
+            }, 101);
+    });
     
     let taskbar_meta_icon = document.createElement("img");
     taskbar_meta_icon.src = env_cfg.taskbar.meta.icon;
--- system/environment/window/everest/init.js
+++ system/environment/window/everest/init.js
@@ -10,14 +10,26 @@ function init() {
     let env_cfg = require("../../../init");
 
     //================================//
+    // Get the window manager element
+    let window_manager = document.querySelector("body > div#window");
+
+    //================================//
     // Get the desktop environment config
     let desktop_env_cfg = require("../../desktop/everest/config.json");
 
     //================================//
     // Get the work area
     let working_area = desktop_env_cfg.window_manager.work_area;
+    let working_area_y_ = working_area.top ? "top" : "bottom";
+    let working_area_x_ = working_area.left ? "left" : "right";
 
-
+    // And append it
+    window_manager.style = `
+        ${working_area_y_}: ${eval(`working_area.${working_area_y_}`)};
+        ${working_area_x_}: ${eval(`working_area.${working_area_x_}`)};
+        width: ${working_area.width};
+        height: ${working_area.height};
+    `;
 }
 
 //================================//
